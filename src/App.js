e051b97408cf49cc4663ea34b968a75ffd67f074
Daniel Machlab <daniel@hadlim.com>
2020-03-23 15:32:52 -0500

    Initial simple site commitd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import herbiePicture from './herbie.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={herbiePicture} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello family!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bit.ly/39hn9FM"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          More Pictures :)
         </a>
       </header>
     </div>
